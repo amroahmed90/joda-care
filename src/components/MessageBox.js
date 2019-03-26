be0bd611bbf41eb53cce4240418da1e9bd16c9e8
Amro Ahmed <amro.utp.pe@gmail.com>
2019-03-26 21:06:12 +0100

    displaying users data in the newsfeed

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -1,43 +1,87 @@
 import React from "react";
+import { Component } from "react";
 
-const MessageBox = props => {
-  return (
-    <div className="container">
-      <div className="row">
-        <div id="outer-box" className="col-sm-10 offset-sm-1">
-          <div id="message-details" className="row">
-            <div id="img-wrap" className="col-3">
-              <a href="#">
-                <img
-                  id="profile-pic"
-                  src="profilepic.jpg"
-                  alt="Profile Pic"
-                  className="border border-secondary rounded-circle"
-                />
-              </a>
-            </div>
-            <div className="col-9">
-              <div id="profile-name" className="row">
-                <a href="#"> Amro Abdelfatah</a>
+import axios from "axios";
+
+class MessageBox extends Component {
+  state = {
+    data: []
+  };
+  componentDidMount() {
+    const username = localStorage.getItem("username");
+    const password = localStorage.getItem("password");
+    const token = localStorage.getItem("token");
+    const tokenBearer = "Bearer " + token;
+    axios({
+      method: "GET",
+      url: "https://jodacare-assignment.herokuapp.com/api/messages",
+      data: {
+        username: username,
+        password: password
+      },
+      headers: {
+        "content-type": "application/jason",
+        Authorization: tokenBearer
+      }
+    }).then(response => {
+      this.setState({ data: response.data.results });
+      console.log(this.state.data);
+    });
+  }
+
+  render() {
+    return (
+      <ul className="container">
+        {this.state.data.map((item, index) => (
+          <li key={index + 1} className="row">
+            <div id="outer-box" className="col-sm-10 offset-sm-1">
+              <div id="message-details" className="row">
+                <div id="img-wrap" className="col-3">
+                  <a href="#">
+                    <img
+                      id="profile-pic"
+                      src={
+                        item.created_by.image
+                          ? item.created_by.image
+                          : "images/unknown-user.png"
+                      }
+                      alt="N/A"
+                      className="border border-secondary rounded-circle"
+                    />
+                  </a>
+                </div>
+                <div className="col-9">
+                  <div id="profile-name" className="row">
+                    <a href="#">
+                      {item.created_by.first_name +
+                        " " +
+                        item.created_by.last_name}
+                    </a>
+                  </div>
+                  <div id="date" className="row">
+                    {"sendt: " + item.created.slice(0, 16).replace("T", " at ")}
+                  </div>
+                </div>
               </div>
-              <div id="date" className="row">
-                sent: 26/03/2019
+              <p id="message-text" className="bordr-top">
+                {item.text}
+              </p>
+              <div id="message-bottom">
+                <a href="#">
+                  <img
+                    id="comment-icon"
+                    src="images/comment-icon.png"
+                    alt="NA"
+                  />
+                  <p id="comments-number">{item.num_comments}</p>
+                </a>
               </div>
             </div>
-          </div>
-          <p id="message-text" className="bordr-top">
-            Here is the message text!
-          </p>
-          <div id="message-bottom">
-            <a href="#">
-              <img id="comment-icon" src="images/comment-icon.png" alt="NA" />
-              <p id="comments-number">2</p>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 export default MessageBox;
--- src/layouts/SignedInLayout.js
+++ src/layouts/SignedInLayout.js
@@ -5,27 +5,7 @@ import MessageBox from "../components/MessageBox";
 
 class SignedInLayout extends Component {
   state = {};
-  getUserData = () => {
-    const username = localStorage.getItem("username");
-    const password = localStorage.getItem("password");
-    const token = localStorage.getItem("token");
-    const tokenBearer = "Bearer " + token;
-    axios({
-      method: "GET",
-      url: "https://jodacare-assignment.herokuapp.com/api/messages",
-      data: {
-        username: username,
-        password: password
-      },
-      headers: {
-        "content-type": "application/jason",
-        Authorization: tokenBearer
-      }
-    }).then(response => {
-      this.setState({ data: response.data.results });
-      console.log(response.data.results);
-    });
-  };
+
   render() {
     return (
       <div>
